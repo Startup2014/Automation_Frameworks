43b8396c097115187472b7414985ff2858c7a767
swatidhoke <swatidhoke@gmail.com>
2015-01-28 19:07:07 -0800

    Added Registration page test steps
    Added Registration page test steps

--- UI_Selenium_framework/Tests/maysRegistration.py
+++ UI_Selenium_framework/Tests/maysRegistration.py
@@ -5,7 +5,9 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
+
 import unittest, time, re
+#from Automation_Frameworks.UI_Selenium_framework.Common_utils import model
 
 class MaysRegistration(unittest.TestCase):
     def setUp(self):
@@ -14,32 +16,22 @@ class MaysRegistration(unittest.TestCase):
         self.base_url = "http://www.macys.com/"
         self.verificationErrors = []
         self.accept_next_alert = True
-    
+
+
     def test_mays_registration(self):
         print "=======Inside registration page======"
+        self.driver.get(self.base_url )
+
+        #click elemantsign in
+        SigninlinkID = "globalMastheadSignIn"
+        CreateProfileID = "createProfileContainer"
+
+        self.driver.find_element_by_id(SigninlinkID).click()
+        self.driver.implicitly_wait(30)
+        self.driver.find_element_by_id(CreateProfileID).click()
         driver = self.driver
-    
-    def is_element_present(self, how, what):
-        try: self.driver.find_element(by=how, value=what)
-        except NoSuchElementException, e: return False
-        return True
-    
-    def is_alert_present(self):
-        try: self.driver.switch_to_alert()
-        except NoAlertPresentException, e: return False
-        return True
-    
-    def close_alert_and_get_its_text(self):
-        try:
-            alert = self.driver.switch_to_alert()
-            alert_text = alert.text
-            if self.accept_next_alert:
-                alert.accept()
-            else:
-                alert.dismiss()
-            return alert_text
-        finally: self.accept_next_alert = True
-    
+
+
     def tearDown(self):
         self.driver.quit()
         self.assertEqual([], self.verificationErrors)
