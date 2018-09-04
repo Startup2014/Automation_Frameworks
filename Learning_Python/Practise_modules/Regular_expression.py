4d1ebad04fcf522cb9deeb246b71e3a1210e4cb1
swatidhoke <swati.dhoke@syapse.com>
2018-09-03 17:15:56 -0700

    Update Regular_expression.py

--- Learning_Python/Practise_modules/Regular_expression.py
+++ Learning_Python/Practise_modules/Regular_expression.py
@@ -1,3 +1,17 @@
 __author__ = 'swatidhoke'
 
 
+regex:http:
+//www.pyregex.com/
+https://pythex.org/
+    
+binary trees, linkedlist,
+Todo:java, slenium, api modules
+
+    
+def regexpression(regex_string_email):
+    retrun True
+    
+    
+    
+    
